70777903d98e417f9cf43e96155b8c3ac3be73cb
Recep <16542010@firat.edu.tr>
2020-10-22 19:43:30 +0300

    for deploy

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    This is Home component
+    <div class="row">
+      <div class="col-sm-12 col-md-1 col-lg-3  bg-danger">
+a
+      </div>
+      <div class="col-sm-4 col-md-5 col-lg-3 bg-dark text-white">s</div>
+      <div class="col-sm-2 col-md-2 col-lg-3 bg-info">d</div>
+      <div class="col-sm-6 col-md-4 col-lg-3 bg-success">e</div>
+    </div>
   </div>
 </template>
 
